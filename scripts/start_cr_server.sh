a6d86875a31c64092c29e5e0ec7dc8980077e22f
GreatAndYoung <41297849+csikeltomi@users.noreply.github.com>
2025-03-30 21:22:10 +0200

    Update start_cr_server.sh

--- scripts/start_cr_server.sh
+++ scripts/start_cr_server.sh
@@ -19,7 +19,12 @@ function download_cr {
         echo "About to download the $cr_version version of the addon."
 
         # ... use the token for downloading the addon package useing the dedicated 'latest' URL
-        curl -# -H "Authorization: Bearer $token" -X GET https://discovery.crowd-render.com/api/download/addon/$cr_version -o crowdrender.zip
+        cr_zip="${cr_version}.zip"
+        
+        curl -# \
+          -H "Authorization: Bearer $token" \
+          -X GET "https://discovery.crowd-render.com/api/download/addon/$cr_zip" \
+          -o "/CR/crowdrender.zip"
 
     fi
 }
